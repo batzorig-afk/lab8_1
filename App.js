714060104bb74eda7f74a1c2d22d440eb8866b59
B200910022 <99472749+batzorig-afk@users.noreply.github.com>
2022-12-05 13:41:05 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, AppRegistry } from 'react-native';
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  View:{
+    margin: 50
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30
+  },
+  red: {
+    color: 'red',
   },
 });
+export default class LotsOfStyles extends Component {
+  render() {
+    return (
+    <View style={styles.View}>
+      <Text style={styles.red}>
+        just red
+      </Text>
+      <Text style={styles.bigBlue}>
+        just bigBlue
+      </Text>
+      <Text style={[styles.bigBlue, styles.red]}>
+        bigBlue, then red
+      </Text>
+      <Text style={[styles.red, styles.bigBlue]}>
+        red, then bigBlue
+      </Text>
+    </View>
+    );
+  }
+}
+AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
